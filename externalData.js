652f4e8b1cd83412adad6eb112807c43fc438904
Gerasim Peshev <gerasimpeshev@gmail.com>
2021-05-26 22:17:11 +0200

    Update externalData.js

--- externalData.js
+++ externalData.js
@@ -7,4 +7,4 @@ export const privateKeys = {
 export const dbHost = 'https://parseapi.back4app.com/classes/tasks';
 export const ipRecordHost = 'https://parseapi.back4app.com/classes/ip_records';
 
-export const allowedIpAddresses = ["193.68.17.109", "77.78.6.194", "109.120.221.29"];
+export const allowedIpAddresses = ["193.68.17.109", "77.78.6.194", "109.120.221.29", "85.91.137.84"];
